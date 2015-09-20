37d18cce11527a0a2660816229df9cb84c54aee2
Víctor Cañada Ojeda <victorcanoje@gmail.com>
2015-09-20 23:12:22 +0100

    CriminalIntent app first challenge complete For a more advanced challenge, create a format string that will display the day of the week as well – for example, “Wednesday, Jul 22, 2015.”

---
+++ app/src/main/java/com/canada/victor/criminalintent/CrimeFragment.java
@@ -0,0 +1,80 @@
+package com.canada.victor.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by Víctor Cañada Ojeda on 20/9/15.
+ * victorcanoje@gmail.com
+ */
+
+
+public class CrimeFragment extends Fragment {
+    //Wednesday, Jul 22, 2015.
+    private static final String DATE_FORMAT = "EEEE, MMM d, yyyy";
+    private SimpleDateFormat df;
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+        df = new SimpleDateFormat(DATE_FORMAT);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //This one too
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+
+        mDateButton.setText(df.format(mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //Set the crime's solved property
+                mCrime.setSolved((isChecked));
+            }
+        });
+
+        return v;
+    }
+}
